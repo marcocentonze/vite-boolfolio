71a1d3ae500d6f5205362ffd31b2ecc74da5f23c
marcocentonze <marco.centonze27@gmail.com>
2023-11-17 18:10:08 +0100

    initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+
+    return {
+
+    }
+
+  },
+
+  methods: {
+
+  },
+
+  mounted() {
+
+  }
+
+}
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped></style>
