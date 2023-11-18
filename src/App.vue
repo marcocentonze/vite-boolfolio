15a3146714429a261da7efd731efa6ee84c03e6b
marcocentonze <marco.centonze27@gmail.com>
2023-11-18 11:37:27 +0100

    add links to cards and footer

--- src/App.vue
+++ src/App.vue
@@ -95,8 +95,16 @@ export default {
           <div class="card shadow w-100">
             <img :src="project.cover_image" class="card-img-top" :alt="project.title">
             <div class="card-body">
-              <h5 class="card-title">{{ project.title }}</h5>
+              <h4 class="card-title">{{ project.title }}</h4>
               <p class="card-text">{{ project.description }}</p>
+              <div class="card-links">
+                <a :href="project.github_link" class="card-link btn btn-dark">
+                  <i class="fa-brands fa-github"></i> GitHub
+                </a>
+                <a :href="project.website_link" class="card-link btn"  style="background-color: #e9ecef">
+                  <i class="fas fa-globe" style="color:#007bff"></i> Website
+                </a>
+              </div>
             </div>
           </div>
         </div>
@@ -104,6 +112,65 @@ export default {
     </div>
 
   </main>
+
+  <footer class="bg-dark text-white pt-4">
+    <div class="container text-center text-md-left">
+        <div class="row text-center text-md-left">
+            <!-- First column -->
+            <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold ">About Me</h5>
+                <p>I am a passionate Junior Full Stack Web Developer, committed to crafting unique and responsive web solutions. This portfolio is all about my adventure with Boolean, diving into loads of cool projects. It's where I got to really play around with and sharpen my skills in both front-end and back-end tech. Check out what I've been up to!</p>
+            </div>
+
+            <!-- Second column -->
+            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold">Projects</h5>
+                <p><a href="#!" class="text-white">Project 1</a></p>
+                <p><a href="#!" class="text-white">Project 2</a></p>
+                <p><a href="#!" class="text-white">Project 3</a></p>
+                <p><a href="#!" class="text-white">Project 4</a></p>
+            </div>
+
+            <!-- Third column -->
+            <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
+                <h5 class="text-uppercase mb-4 font-weight-bold">Contact</h5>
+                <p><i class="fas fa-home mr-3"></i> Lecce</p>
+                <p><i class="fas fa-envelope mr-3"></i> marco.centonze27@gmail.com</p>
+                <p><i class="fas fa-phone mr-3"></i> + 123 32 23 531</p>
+               
+            </div>
+        </div>
+
+        <hr class="mb-4">
+
+        <!-- Social media links -->
+        <div class="row align-items-center">
+            <div class="col-md-7 col-lg-8">
+                <p>&copy; 2023 Marco Centonze. All Rights Reserved.</p>
+            </div>
+
+            <div class="col-md-5 col-lg-4">
+                <div class="text-center text-md-right">
+                    <ul class="list-unstyled list-inline">
+                        <li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-facebook-f"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-twitter"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-instagram"></i></a>
+                        </li>
+                        <li class="list-inline-item">
+                            <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-linkedin-in"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
 </template>
 
 <style scoped></style>
