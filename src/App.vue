d9219dc1b09515d97d5a60a3f0c4ee716b5c0801
marcocentonze <marco.centonze27@gmail.com>
2023-11-19 20:41:06 +0100

    add ProjectCard component

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,26 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProjectCard from './components/ProjectCard.vue'
 import axios from 'axios';
 
 export default {
   name: 'App',
+  components: {
+        ProjectCard,
+       
+    },
   data() {
     return {
-      base_url: 'http://127.0.0.1:8000/',
-      projects_api: 'api/projects',
-      projects: [],
+     
     }
 
   },
 
   methods: {
-    getProjects() {
-      const url = this.base_url + this.projects_api;
-      axios
-        .get(url)
-        .then(response => {
-          console.log(response);
-          this.projects = response.data.result
-        })
-        .catch(err => {
-          console.error(err);
-        })
-    }
+  
   },
 
   mounted() {
-    this.getProjects();
+    
   }
 
 }
@@ -78,99 +69,91 @@ export default {
   </header>
 
   <!-- jumbotron -->
-  <div class="p-5 mb-4 bg-light rounded-3">
-    <div class="container-fluid py-5">
-      <h1 class="display-5 fw-bold">Custom jumbotron</h1>
-      <p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous
-        versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-      <button class="btn btn-primary btn-lg" type="button">Example button</button>
+
+  <div class="p-5 mb-4 bg-light rounded-3 custom-jumbo-img">
+    <div class="container-fluid py-5 text-white">
+      <h1 class="display-5 fw-bold">My Portfolio</h1>
+      <p class="col-md-8 fs-4">This is my portfolio where you will find both front-end and back-end projects for about the last 5 months.</p>
+      <button class="btn btn-primary btn-lg" type="button">Discover more!</button>
     </div>
   </div>
-
+ 
   <!-- cards -->
-  <main>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4 mb-4 d-flex align-items-stretch" v-for="project in projects.data">
-          <div class="card shadow w-100">
-            <img :src="project.cover_image" class="card-img-top" :alt="project.title">
-            <div class="card-body">
-              <h4 class="card-title">{{ project.title }}</h4>
-              <p class="card-text">{{ project.description }}</p>
-              <div class="card-links">
-                <a :href="project.github_link" class="card-link btn btn-dark">
-                  <i class="fa-brands fa-github"></i> GitHub
-                </a>
-                <a :href="project.website_link" class="card-link btn"  style="background-color: #e9ecef">
-                  <i class="fas fa-globe" style="color:#007bff"></i> Website
-                </a>
-              </div>
-            </div>
+ <ProjectCard></ProjectCard>
+  <!-- footer -->
+  
+
+  <footer class="bg-dark text-white pt-4">
+    <div class="container text-center text-md-left">
+      <div class="row text-center text-md-left">
+        <!-- First column -->
+        <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold ">About Me</h5>
+          <p>I am a passionate Junior Full Stack Web Developer, committed to crafting unique and responsive web solutions.
+            This portfolio is all about my adventure with Boolean, diving into loads of cool projects. It's where I got to
+            really play around with and sharpen my skills in both front-end and back-end tech. Check out what I've been up
+            to!</p>
+        </div>
+
+        <!-- Second column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold">Projects</h5>
+          <p><a href="#!" class="text-white">Project 1</a></p>
+          <p><a href="#!" class="text-white">Project 2</a></p>
+          <p><a href="#!" class="text-white">Project 3</a></p>
+          <p><a href="#!" class="text-white">Project 4</a></p>
+        </div>
+
+        <!-- Third column -->
+        <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
+          <h5 class="text-uppercase mb-4 font-weight-bold">Contact</h5>
+          <p><i class="fas fa-home mr-3"></i> Lecce</p>
+          <p><i class="fas fa-envelope mr-3"></i> marco.centonze27@gmail.com</p>
+          <p><i class="fas fa-phone mr-3"></i> + 123 32 23 531</p>
+
+        </div>
+      </div>
+
+      <hr class="mb-4">
+
+      <!-- Social media links -->
+      <div class="row align-items-center">
+        <div class="col-md-7 col-lg-8">
+          <p>&copy; 2023 Marco Centonze. All Rights Reserved.</p>
+        </div>
+
+        <div class="col-md-5 col-lg-4">
+          <div class="text-center text-md-right">
+            <ul class="list-unstyled list-inline">
+              <li class="list-inline-item">
+                <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i
+                    class="fab fa-facebook-f"></i></a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i
+                    class="fab fa-twitter"></i></a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i
+                    class="fab fa-instagram"></i></a>
+              </li>
+              <li class="list-inline-item">
+                <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i
+                    class="fab fa-linkedin-in"></i></a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
     </div>
-
-  </main>
-
-  <footer class="bg-dark text-white pt-4">
-    <div class="container text-center text-md-left">
-        <div class="row text-center text-md-left">
-            <!-- First column -->
-            <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
-                <h5 class="text-uppercase mb-4 font-weight-bold ">About Me</h5>
-                <p>I am a passionate Junior Full Stack Web Developer, committed to crafting unique and responsive web solutions. This portfolio is all about my adventure with Boolean, diving into loads of cool projects. It's where I got to really play around with and sharpen my skills in both front-end and back-end tech. Check out what I've been up to!</p>
-            </div>
-
-            <!-- Second column -->
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
-                <h5 class="text-uppercase mb-4 font-weight-bold">Projects</h5>
-                <p><a href="#!" class="text-white">Project 1</a></p>
-                <p><a href="#!" class="text-white">Project 2</a></p>
-                <p><a href="#!" class="text-white">Project 3</a></p>
-                <p><a href="#!" class="text-white">Project 4</a></p>
-            </div>
-
-            <!-- Third column -->
-            <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mt-3">
-                <h5 class="text-uppercase mb-4 font-weight-bold">Contact</h5>
-                <p><i class="fas fa-home mr-3"></i> Lecce</p>
-                <p><i class="fas fa-envelope mr-3"></i> marco.centonze27@gmail.com</p>
-                <p><i class="fas fa-phone mr-3"></i> + 123 32 23 531</p>
-               
-            </div>
-        </div>
-
-        <hr class="mb-4">
-
-        <!-- Social media links -->
-        <div class="row align-items-center">
-            <div class="col-md-7 col-lg-8">
-                <p>&copy; 2023 Marco Centonze. All Rights Reserved.</p>
-            </div>
-
-            <div class="col-md-5 col-lg-4">
-                <div class="text-center text-md-right">
-                    <ul class="list-unstyled list-inline">
-                        <li class="list-inline-item">
-                            <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-facebook-f"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-twitter"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-instagram"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="#" class="btn-floating btn-sm text-white" style="font-size: 23px;"><i class="fab fa-linkedin-in"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </div>
-</footer>
-
+  </footer>
 </template>
 
-<style scoped></style>
+<style>
+.custom-jumbo-img {
+  background-image: url('./assets/images/jumbotron.png');
+  background-size: cover;
+  background-position: center;
+  height: 500px;
+}
+</style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,66 @@
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'ProjectCard',
+  data() {
+
+    return {
+      base_url: 'http://127.0.0.1:8000/',
+      projects_api: 'api/projects',
+      projects: [],
+    }
+
+  },
+
+  methods: {
+    getProjects() {
+      const url = this.base_url + this.projects_api;
+      axios
+        .get(url)
+        .then(response => {
+          console.log(response);
+          this.projects = response.data.result
+        })
+        .catch(err => {
+          console.error(err);
+        })
+    }
+  },
+
+  mounted() {
+    this.getProjects();
+  }
+
+}
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mb-4 d-flex align-items-stretch" v-for="project in projects.data">
+          <div class="card shadow w-100">
+            <img :src="project.cover_image" class="card-img-top" :alt="project.title">
+            <div class="card-body">
+              <h4 class="card-title">{{ project.title }}</h4>
+              <p class="card-text">{{ project.description }}</p>
+
+              <div class="card-links">
+                <a :href="project.github_link" class="card-link btn btn-dark">
+                  <i class="fa-brands fa-github"></i> GitHub
+                </a>
+                <a :href="project.website_link" class="card-link btn" style="background-color: #e9ecef">
+                  <i class="fas fa-globe" style="color:#007bff"></i> Website
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </main>
+</template>
+
+<style scoped></style>
