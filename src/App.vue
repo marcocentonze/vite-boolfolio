2c6454eb05c1d98017efb8e46e502dbb42a53226
marcocentonze <marco.centonze27@gmail.com>
2023-11-20 15:38:08 +0100

    add AppHeader component + vue router installation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script>
-import ProjectCard from './components/ProjectCard.vue'
+import ProjectCard from './components/ProjectCard.vue';
+import AppHeader from './components/AppHeader.vue'
 import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
         ProjectCard,
+        AppHeader,
        
     },
   data() {
@@ -27,46 +29,7 @@ export default {
 </script>
 
 <template>
-  <!-- navbar -->
-  <header>
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-      <div class="container">
-        <a class="navbar-brand" href="#">Navbar</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
-          data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" href="#" aria-current="page">Home <span
-                  class="visually-hidden">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true"
-                aria-expanded="false">Dropdown</a>
-              <div class="dropdown-menu" aria-labelledby="dropdownId">
-                <a class="dropdown-item" href="#">Action 1</a>
-                <a class="dropdown-item" href="#">Action 2</a>
-              </div>
-            </li>
-          </ul>
-          <form class="d-flex my-2 my-lg-0">
-            <input class="form-control me-sm-2" type="text" placeholder="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-          </form>
-          <!-- Pulsante Admin -->
-          <div class="ms-auto">
-            <a href="http://127.0.0.1:8000/" class="btn btn-primary">Admin</a>
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
+ <AppHeader></AppHeader>
 
   <!-- jumbotron -->
 
@@ -80,6 +43,7 @@ export default {
  
   <!-- cards -->
  <ProjectCard></ProjectCard>
+ 
   <!-- footer -->
   
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,59 @@
+
+
+<script>
+    export default {
+        name: 'AppHeader',
+        data() {
+            return {
+      
+    }
+    },
+    methods: {
+        
+      },
+}
+</script>
+
+
+<template>
+     <!-- navbar -->
+  <header>
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand text-danger" href="#">Boolfolio</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+          data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
+          aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" href="#" aria-current="page">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contacts</a>
+            </li>
+           
+          </ul>
+          <form class="d-flex my-2 my-lg-0">
+            <input class="form-control me-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+          <!-- Pulsante Admin -->
+          <div class="ms-auto">
+            <a href="http://127.0.0.1:8000/" class="btn btn-primary">Admin</a>
+          </div>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
